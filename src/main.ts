8b75af779ea0f9406bc4f4389444babc27f0ad1a
Dark Shell <134949053+darkshell86@users.noreply.github.com>
2024-03-10 15:25:47 +0000

    fix: svg keys and add right escape animation

--- src/main.ts
+++ src/main.ts
@@ -15,28 +15,41 @@ async function greet() {
 
 await invoke("init_process")
 
+const keyPressStyle = (key) => {
+  document.querySelector(`#key-${key}--inject-1`).style.transition = "100ms";
+  // document.querySelector("#key-outline-f--inject-1").style.fill = "#83EEFF";
+  document.querySelector(`#key-inline-${key}--inject-1`).style.fill = "#938E73";
+  document.querySelector(`#key-outline-${key}--inject-1`).style.fill = "#5B4D02";
+  document.querySelector(`#key-${key}--inject-1`).style.transform = "scale(1, 0.8)";
+  document.querySelector(`#key-${key}--inject-1`).style.transformOrigin = "50% 60%";
+  document.querySelector(`#key-inline-${key}--inject-1`).style.filter = "brightness(100%)"
+}
+
+const keyReleaseStyle = (key) => {
+  document.querySelector(`#key-${key}--inject-1`).style.transition = "100ms";
+  document.querySelector(`#key-inline-${key}--inject-1`).style.fill = "#f9f4d9";
+  document.querySelector(`#key-outline-${key}--inject-1`).style.fill = "#c1b368";
+  document.querySelector(`#key-${key}--inject-1`).style.transform = "scale(1, 1)";
+  document.querySelector(`#key-${key}--inject-1`).style.transformOrigin = "50% 50%";
+  document.querySelector(`#key-${key}--inject-1`).style.filter = "brightness(80%)"
+}
+
 const unlisten = await listen('key', (event) => {
-console.log(event.payload.message.str)
-document.querySelector("#key-f--inject-1").style.transition = "100ms";
+console.log(event.payload.message)
   if (event.payload.message.match("KeyPress")) {
     console.log('F got pressed')
-    const key = event.payload.message.replace(/KeyPress-/,'').replace(/Key/,'').toLowerCase()
-    document.querySelector(`#key-${key}--inject-1`).style.transition = "100ms";
-    // document.querySelector("#key-outline-f--inject-1").style.fill = "#83EEFF";
-    document.querySelector(`#key-inline-${key}--inject-1`).style.fill = "#938E73";
-    document.querySelector(`#key-outline-${key}--inject-1`).style.fill = "#5B4D02";
-    document.querySelector(`#key-${key}--inject-1`).style.transform = "scale(1, 0.8)";
-    document.querySelector(`#key-${key}--inject-1`).style.transformOrigin = "50% 60%";
-    document.querySelector(`#key-inline-${key}--inject-1`).style.filter = "brightness(100%)"
+    const key = event.payload.message.replace(/KeyPress-/,'')
+    if (key == 'Escape') {
+      keyPressStyle('RightEscape')
+    }
+    keyPressStyle(key)
   }
   if (event.payload.message.match("KeyRelease")) {
-    const key = event.payload.message.replace(/KeyRelease-/,'').replace(/Key/,'').toLowerCase()
-    document.querySelector(`#key-${key}--inject-1`).style.transition = "100ms";
-    document.querySelector(`#key-inline-${key}--inject-1`).style.fill = "#f9f4d9";
-    document.querySelector(`#key-outline-${key}--inject-1`).style.fill = "#c1b368";
-    document.querySelector(`#key-${key}--inject-1`).style.transform = "scale(1, 1)";
-    document.querySelector(`#key-${key}--inject-1`).style.transformOrigin = "50% 50%";
-    document.querySelector(`#key-${key}--inject-1`).style.filter = "brightness(80%)"
+    const key = event.payload.message.replace(/KeyRelease-/,'')
+    if (key == 'Escape') {
+      keyReleaseStyle('RightEscape')
+    }
+    keyReleaseStyle(key)
   }
   console.log(event.payload)
 })
