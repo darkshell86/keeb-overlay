93e24c2cab9336fd55e9f7f277b8c32bcb8ff275
Dark Shell <134949053+darkshell86@users.noreply.github.com>
2024-03-17 14:23:05 +0000

    fix some errors

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,15 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use rdev::{Event, EventType};
 use std::thread;
-use rdev::{listen, Event, EventType, Key};
 
 use tauri::{Manager, Window};
 
 // the payload type must implement `Serialize` and `Clone`.
 #[derive(Clone, serde::Serialize)]
 struct Payload {
-  message: String,
+    message: String,
 }
 
 // init a background process on the command, and emit periodic events only to the window that used the command
@@ -17,10 +17,24 @@ struct Payload {
 fn init_process(window: Window) {
     let callback = move |event: Event| {
         match event.event_type {
-            EventType::KeyPress(key) => window.emit("key", Payload { message: format!("KeyPress-{:?}", key) }).unwrap(),
+            EventType::KeyPress(key) => window
+                .emit(
+                    "key",
+                    Payload {
+                        message: format!("KeyPress-{:?}", key),
+                    },
+                )
+                .unwrap(),
             // EventType::KeyRelease(key) => println!("My callback {:?}", event.event_type),
-            EventType::KeyRelease(key) => window.emit("key", Payload { message: format!("KeyRelease-{:?}", key) }).unwrap(),
-            EventType::MouseMove{x, y}=> (),
+            EventType::KeyRelease(key) => window
+                .emit(
+                    "key",
+                    Payload {
+                        message: format!("KeyRelease-{:?}", key),
+                    },
+                )
+                .unwrap(),
+            // EventType::MouseMove{x, y}=> (),
             _ => println!("My callback {:?}", event.event_type),
         }
         // if let EventType::KeyPress(Key::KeyF) = event.event_type {
@@ -37,32 +51,38 @@ fn init_process(window: Window) {
         // };
     };
 
-    
     thread::spawn(move || {
         rdev::listen(callback).expect("could not listen events");
     });
 }
 
 fn main() {
-  tauri::Builder::default()
-    .setup(|app| {
-      // `main` here is the window label; it is defined on the window creation or under `tauri.conf.json`
-      // the default value is `main`. note that it must be unique
-      let main_window = app.get_window("main").unwrap();
+    tauri::Builder::default()
+        .setup(|app| {
+            // `main` here is the window label; it is defined on the window creation or under `tauri.conf.json`
+            // the default value is `main`. note that it must be unique
+            let main_window = app.get_window("main").unwrap();
 
-      // listen to the `event-name` (emitted on the `main` window)
-      let id = main_window.listen("event-name", |event| {
-        println!("got window event-name with payload {:?}", event.payload());
-      });
-      // unlisten to the event using the `id` returned on the `listen` function
-      // an `once` API is also exposed on the `Window` struct
-      main_window.unlisten(id);
+            // listen to the `event-name` (emitted on the `main` window)
+            let id = main_window.listen("event-name", |event| {
+                println!("got window event-name with payload {:?}", event.payload());
+            });
+            // unlisten to the event using the `id` returned on the `listen` function
+            // an `once` API is also exposed on the `Window` struct
+            main_window.unlisten(id);
 
-      // emit the `event-name` event to the `main` window
-      main_window.emit("event-name", Payload { message: "Tauri is awesome!".into() }).unwrap();
-      Ok(())
-    })
-    .invoke_handler(tauri::generate_handler![init_process])
-    .run(tauri::generate_context!())
-    .expect("failed to run app");
+            // emit the `event-name` event to the `main` window
+            main_window
+                .emit(
+                    "event-name",
+                    Payload {
+                        message: "Tauri is awesome!".into(),
+                    },
+                )
+                .unwrap();
+            Ok(())
+        })
+        .invoke_handler(tauri::generate_handler![init_process])
+        .run(tauri::generate_context!())
+        .expect("failed to run app");
 }
--- src/main.ts
+++ src/main.ts
@@ -12,30 +12,49 @@ async function greet() {
     });
   }
 }
-
+const main = async () => {
 await invoke("init_process")
 
-const keyPressStyle = (key) => {
-  document.querySelector(`#key-${key}--inject-1`).style.transition = "100ms";
-  // document.querySelector("#key-outline-f--inject-1").style.fill = "#83EEFF";
-  document.querySelector(`#key-inline-${key}--inject-1`).style.fill = "#938E73";
-  document.querySelector(`#key-outline-${key}--inject-1`).style.fill = "#5B4D02";
-  document.querySelector(`#key-${key}--inject-1`).style.transform = "scale(1, 0.8)";
-  document.querySelector(`#key-${key}--inject-1`).style.transformOrigin = "50% 60%";
-  document.querySelector(`#key-inline-${key}--inject-1`).style.filter = "brightness(100%)"
+const keyPressStyle = (key: string) => {
+  const keyEl = document.querySelector(`#key-${key!}--inject-1`) as HTMLElement
+  const keyInlineEl = document.querySelector(`#key-inline-${key}--inject-1`) as HTMLElement
+  const keyOutlineEl = document.querySelector(`#key-outline-${key}--inject-1`) as HTMLElement
+  
+  if (keyEl) {
+    keyEl.style.transition = "100ms"
+    keyEl.style.transform = "scale(1, 0.8)";
+    keyEl.style.transformOrigin = "50% 60%";
+    keyEl.style.filter = "brightness(100%)"
+  }
+  if (keyInlineEl) {
+    keyInlineEl.style.fill = "#938E73";
+  }
+  if (keyOutlineEl) {
+    keyOutlineEl.style.fill = "#5B4D02";
+  }
 }
 
-const keyReleaseStyle = (key) => {
-  document.querySelector(`#key-${key}--inject-1`).style.transition = "100ms";
-  document.querySelector(`#key-inline-${key}--inject-1`).style.fill = "#f9f4d9";
-  document.querySelector(`#key-outline-${key}--inject-1`).style.fill = "#c1b368";
-  document.querySelector(`#key-${key}--inject-1`).style.transform = "scale(1, 1)";
-  document.querySelector(`#key-${key}--inject-1`).style.transformOrigin = "50% 50%";
-  document.querySelector(`#key-${key}--inject-1`).style.filter = "brightness(80%)"
+const keyReleaseStyle = (key: string) => {
+  const keyEl = document.querySelector(`#key-${key!}--inject-1`) as HTMLElement
+  const keyInlineEl = document.querySelector(`#key-inline-${key}--inject-1`) as HTMLElement
+  const keyOutlineEl = document.querySelector(`#key-outline-${key}--inject-1`) as HTMLElement
+  
+  if (keyEl) {
+    keyEl.style.transition = "100ms"
+    keyEl.style.transform = "scale(1, 1)";
+    keyEl.style.transformOrigin = "50% 50%";
+    keyEl.style.filter = "brightness(80%)"
+  }
+  if (keyInlineEl) {
+    keyInlineEl.style.fill = "#f9f4d9";
+  }
+  if (keyOutlineEl) {
+    keyOutlineEl.style.fill = "#c1b368";
+  }
 }
 
-const unlisten = await listen('key', (event) => {
-console.log(event.payload.message)
+await listen('key', (event: any) => {
+  console.log(event.payload.message)
   if (event.payload.message.match("KeyPress")) {
     console.log('F got pressed')
     const key = event.payload.message.replace(/KeyPress-/,'')
@@ -53,7 +72,8 @@ console.log(event.payload.message)
   }
   console.log(event.payload)
 })
-
+}
+main()
 window.addEventListener("DOMContentLoaded", () => {
   greetInputEl = document.querySelector("#greet-input");
   greetMsgEl = document.querySelector("#greet-msg");
